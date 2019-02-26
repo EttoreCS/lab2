866745e3f09c1cbaf815f4b1a7088008223520ed
Ettore Senatore <47864270+EttoreCS@users.noreply.github.com>
2019-02-26 10:17:44 -0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,10 @@
+package Lab2;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Country ca = new Country();
+        ca.displayAllProvinces();
+        ca.howManyHaveThisPopulation(4,38);
+    }
+}
